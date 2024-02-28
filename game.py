1b9c4db2989002441579016e70c40462d773cd22
samuelms123 <samuel.sarimo@metropolia.fi>
2024-02-28 15:30:22 +0200

    Ohjeistus-funktio luotu sekä aloituskyssäri

--- game.py
+++ game.py
@@ -10,6 +10,42 @@ def game_loop():
         if game_over:
             running = False
 
+def ohjeistus():
+    ohje = """
+Tervetuloa pelaamaan Python Pilottia!
+Pelin tavoitteena on saapua Ankaran lentokentälle mahdollisimman vähin siirroin, lähtöpaikkasi on Helsinki-Vantaan lentokenttä. 
+Yhdellä siirrolla lentokone liikkuu 200km haluttuun ilmansuuntaan.
+Lentokone on täysin tankattu pelin alussa, jolla voit kulkea maksimissaan 1000 kilometriä. Aina kun laskeudut lähimmälle lentokentälle,
+lentokoneen tankki täytetään 100 %.
+
+Pelin näppäimet :
+Num 1: Lounas
+Num 2: Etelä
+Num 3: Kaakko
+Num 4: Länsi
+Num 6: Itä
+Num 7: Luode
+Num 8: Pohjoinen
+Num 9: Koillinen
+SPACE: Avaa valikon, jossa on viisi lähintä lentokenttää. Kentät on numeroitu 1–5. Painamalla halutun lentokentän numeroa, kone laskeutuu
+sinne.
+H: ohjeet ja opasteet
+
+"""
+    print(ohje)
+
+
+ohjeistus()
+
+while True:
+    kysy = input("Oletko valmis aloittamaan?[kyllä = ENTER]: ")
+    if kysy == "":
+        game_loop()
+    else:
+        continue
+
+
+
 
 # Kutsutaan peli looppi.
-game_loop()
+#game_loop()
