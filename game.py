88584ff63c3385c45bca2ec0cf75c8c2547fce86
rikudi <rikukaartoaho@gmail.com>
2024-02-28 13:58:11 +0200

    alustava game.py

---
+++ game.py
@@ -0,0 +1,15 @@
+# Peli ydin-looppi.
+def game_loop():
+    running = True
+    game_over = False
+
+    while running:
+        
+      # pelin logiikkaa tänne
+
+        if game_over:
+            running = False
+
+
+# Kutsutaan peli looppi.
+game_loop()
