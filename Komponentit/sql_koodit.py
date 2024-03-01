bccdb1b787c9851d1a66cb004fff51f1bf0ab22d
Otto <ottoparnanen1@gmail.com>
2024-03-01 12:45:49 +0200

    Tehty polttoaine vähennys koodi ja pelaajan lisääminen tietokantaan koodi

--- Komponentit/sql_koodit.py
+++ Komponentit/sql_koodit.py
@@ -9,3 +9,17 @@ yhteys = mysql.connector.connect(
     password='Lihapullat1Maria',
     autocommit=True
 )
+
+# Funktio polttoaineen vähentämiseen kierroksen jälkeen #
+def mysql_update(nimi):
+    sql = f"UPDATE game SET fuel = fuel - 20 WHERE id = '{nimi}'"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    return
+
+# funktio pelaajan nimen lisäämiseen tietokantaan ja pelaajalle annettaan alkuarvot tietkannas"
+def mysql_insert_into(nimi):
+    sql = f"INSERT INTO game (id, fuel, location_lat, location_lon, kierrokset) VALUES ('{nimi}', 100, 60.3172, 24.963301, 0)"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    return
