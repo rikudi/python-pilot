bf87fcc9658cdc3ec621e1c776596ec9c5637dc0
samuelms123 <samuel.sarimo@metropolia.fi>
2024-03-08 23:20:31 +0200

    Uusi palaute-funktio, joka tallentaa ennätykset

--- Komponentit/palaute.py
+++ Komponentit/palaute.py
@@ -9,6 +9,42 @@ yhteys = mysql.connector.connect(
          password='alakatomunsalasanaa',
          autocommit=True
          )
+'''
+def palaute(pelaaja_id):
+    sql = f"SELECT kierrokset, high_score FROM game WHERE id = '{pelaaja_id}'"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchone()
+    kierrokset = (tulos[0])
+    high_score = (tulos[1])
+    kilometrit = kierrokset * 200
+
+    if high_score == 0 or high_score > kierrokset:
+        print("Uusi ennätys!")
+        if kilometrit <= 2600:
+            print("Matkasi pituus oli", kilometrit, "km! Suorituksesi oli erinomainen!")
+        elif 2600 < kilometrit <= 3200:
+            print("Matkasi pituus oli", kilometrit, "km! Suorituksesi oli hyvä!")
+        elif 3200 < kilometrit <= 4000:
+            print("Matkasi pituus oli", kilometrit, "km! Suorituksesi oli tyydyttävä!")
+        else:
+            print("Matkasi pituus oli", kilometrit, "km! Suorituksesi oli huono!")
+
+        sql_high_score = f"UPDATE game SET high_score = {kierrokset} WHERE id = '{pelaaja_id}'"
+        kursori = yhteys.cursor()
+        kursori.execute(sql_high_score)
+
+    elif high_score < kierrokset:
+        if kilometrit <= 2600:
+            print("Matkasi pituus oli", kilometrit, "km! Suorituksesi oli erinomainen!")
+        elif 2600 < kilometrit <= 3200:
+            print("Matkasi pituus oli", kilometrit, "km! Suorituksesi oli hyvä!")
+        elif 3200 < kilometrit <= 4000:
+            print("Matkasi pituus oli", kilometrit, "km! Suorituksesi oli tyydyttävä!")
+        else:
+            print("Matkasi pituus oli", kilometrit, "km! Suorituksesi oli huono!")
+
+'''
 
 
 def palaute(pelaaja_id):
@@ -27,6 +63,7 @@ def palaute(pelaaja_id):
         print("Matkasi pituus oli", kilometrit, "km! Suorituksesi oli tyydyttävä!")
     else:
         print("Matkasi pituus oli", kilometrit, "km! Suorituksesi oli huono!")
+
 '''
     while True:
         uusi_peli = input("Haluatko pelata uudestaan (kyllä/ei): ").lower()
