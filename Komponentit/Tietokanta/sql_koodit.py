248d8c2ac9ddfebf031e59632405278a27a9730f
samuelms123 <samuel.sarimo@metropolia.fi>
2024-03-13 12:53:06 +0200

    Palaute.py siirretty sql_koodeihin

--- Komponentit/Tietokanta/sql_koodit.py
+++ Komponentit/Tietokanta/sql_koodit.py
@@ -7,7 +7,7 @@ yhteys = mysql.connector.connect(
     port=3306,
     database='python_pilot',  # vaiha
     user='root',
-    password='admin',         # vaiha
+    password='alakatomunsalasanaa',         # vaiha
     autocommit=True
 )
 
@@ -126,3 +126,37 @@ def etaisyys_ankara(pelaaja_id):
     nykyinen_sijainti = (tulos[0][0], tulos[0][1])
     ankara = (40.128101348899996, 32.995098114)
     print("\nEtäisyys määränpäästä:", round(great_circle(nykyinen_sijainti,ankara).kilometers),"km")
+
+def palaute(pelaaja_id):
+    sql = f"SELECT kierrokset, high_score FROM game WHERE id = '{pelaaja_id}'"
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchone()
+    kierrokset = (tulos[0])
+    high_score = (tulos[1])
+    kilometrit = kierrokset * 200
+
+    if high_score == 0 or high_score > kierrokset:
+        print("Uusi ennätys!")
+        if kilometrit <= 2600:
+            print("Matkasi pituus oli", kilometrit, "km! Suorituksesi oli erinomainen!")
+        elif 2600 < kilometrit <= 3200:
+            print("Matkasi pituus oli", kilometrit, "km! Suorituksesi oli hyvä!")
+        elif 3200 < kilometrit <= 4000:
+            print("Matkasi pituus oli", kilometrit, "km! Suorituksesi oli tyydyttävä!")
+        else:
+            print("Matkasi pituus oli", kilometrit, "km! Suorituksesi oli huono!")
+
+        sql_high_score = f"UPDATE game SET high_score = {kierrokset} WHERE id = '{pelaaja_id}'"
+        kursori = yhteys.cursor()
+        kursori.execute(sql_high_score)
+
+    elif high_score < kierrokset:
+        if kilometrit <= 2600:
+            print("Matkasi pituus oli", kilometrit, "km! Suorituksesi oli erinomainen!")
+        elif 2600 < kilometrit <= 3200:
+            print("Matkasi pituus oli", kilometrit, "km! Suorituksesi oli hyvä!")
+        elif 3200 < kilometrit <= 4000:
+            print("Matkasi pituus oli", kilometrit, "km! Suorituksesi oli tyydyttävä!")
+        else:
+            print("Matkasi pituus oli", kilometrit, "km! Suorituksesi oli huono!")
